918a7ef640aee411802d972373406204a4b08ef3
muflub <admin@lodle.net>
2017-11-19 19:13:21 -0800

    Update 1_Authentication.cpp

--- examples/1_Authentication.cpp
+++ examples/1_Authentication.cpp
@@ -1,65 +1,86 @@
 #include "ModIOSDK.h"
 
-bool email_request_finished = false;
-bool email_exchange_finished = false;
+#include <iostream>
+#include <string>
 
-void onEmailRequest(ModioResponse* response, char* message)
+int main()
 {
-  cout<<"Response code: "<<response->code<<endl;
-  if(response->code == 200)
+  modio::Instance mod(7, "e91c01b8882f4affeddd56c96111977b");
+  
+  volatile bool finished = false;
+  
+  auto wait = [&]()
   {
-    cout<<"Message: "<<message<<endl;
-  }else
-  {
-    cout<<"Error sending code"<<endl;
-  }
-  email_request_finished = true;
-}
-
-void onExchange(ModioResponse* response)
-{
-  cout<<"Response code: "<<response->code<<endl;
-  if(response->code == 200)
-  {
-    cout<<"Code exchanged!"<<endl;
-  }else
-  {
-    cout<<"Error exchanging code"<<endl;
-  }
-  email_exchange_finished = true;
-}
-
-int main(void)
-{
-  modioInit(7, (char*)"e91c01b8882f4affeddd56c96111977b");
-
-  if(!modioIsLoggedIn())
-  {
-    string email;
-    cout<<"Enter your email: "<<endl;
-    cin>>email;
-    modioEmailRequest((char*)email.c_str(),&onEmailRequest);
-    while(!email_request_finished);
-    string security_code;
-    cout<<"Please enter the 5 digit security code: ";
-    cin>>security_code;
-    cout<<"Sending code"<<endl;
-    modioEmailExchange((char*)security_code.c_str(),&onExchange);
-    while(!email_exchange_finished);
-  }else
-  {
-    cout<<"You are already logged in. Do you want to logout? (y/n)"<<endl;
-    string user_option;
-    cin>>user_option;
-    if(user_option == "y")
+    while (!finished)
     {
-      modioLogout();
+      Sleep(10);
+    }
+  };
+  
+  auto finish = [&]()
+  {
+    finished = true;
+  };
+  
+  // Check to see if we have a cookie and are already logged in
+  if (!mod.IsLoggedIn())
+  {
+    std::string email;
+    
+    std::cout << "Enter your email: " << std::endl;
+    std::cin >> email;
+    
+    // Auth works by sending an email with a code. Lets trigger that now
+    mod.emailRequest(email, [&](const ModioResponse& response, const std::string& message)
+    {
+      std::cout << "Response code: " << response.code << std::endl;
+      
+      if (response->code == 200)
+      {
+        std::cout << "Message: " << message << std::endl;
+        
+        std::string securityCode;
+        std::cout << "Please enter the 5 digit security code: ";
+        std::cin >> securityCode;
+        
+        // Finish the auth process by entering the security code
+        mod.emailExchange(securityCode, [&](const ModioResponse& response)
+        {
+          std::cout << "Response code: " << response.code << std::endl;
+  
+          if (response->code == 200)
+          {
+            std::cout << "Code exchanged!" << std::endl;
+          }
+          else
+          {
+            std::cout << "Error exchanging code" << std::endl;
+          }
+          
+          finish();
+        });
+      }
+      else
+      {
+        std::cout << "Error sending code" << endl;
+        finish();
+      }
+    });
+    
+    wait();
+  }
+  else
+  {
+    std::cout << "You are already logged in. Do you want to logout? (y/n)" << std::endl;
+    std::string userOption;
+    std::cin >> userOption;
+    
+    if (userOption == "y")
+    {
+      mod.logout();
     }
   }
 
-  modioShutdown();
-
-  cout<<"Process finished"<<endl;
-
+  std::cout << "Process finished" << std::endl;
   return 0;
 }
